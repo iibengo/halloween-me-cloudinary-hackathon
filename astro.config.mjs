6556bbe5d5d8d1f94761cf9b37763c535ca68653
ion <ionilancer@gmail.com>
2024-10-13 11:10:11 +0200

    vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import fulldev from 'fulldev-ui/integration'
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
@@ -11,16 +10,7 @@ export default defineConfig({
   experimental: {
     contentLayer: true
   },
-  integrations: [fulldev({
-    colors: {
-      theme: 'dark',
-      dark: {
-        background: '#1f1f1f',
-        base: '#6f6d66',
-        brand: '#f50'
-      }
-    }
-  }), vue(), tailwind()],
+  integrations: [ vue(), tailwind()],
   adapter: vercel(), 
 
 });
